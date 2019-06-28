acefc430ff9ce44097ab25ce49f74631d5870a19
alec-mccormick <alec.s.mccormick@gmail.com>
2019-06-28 14:32:11 -0700

    Initialization

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Cursor-Pagination
-Page token generator for javascript
+Page token generator for javascript using Protobufjs
+
+Example Usage:
+```javascript
+const { CursorPagination } = require('cursor-pagination');
+const pagination = new CursorPagination();
+
+// Returns a buffer
+const tokenData = pagination.createToken({
+  entries: [
+    { key: 'updatedAt', value: new Date() },
+    { key: 'id', value: 'abcdefg', asc: false }
+  ]
+});
+
+// Convert to base64 string to return to user.
+const token = tokenData.toString('base64');
+
+// Convert token back into buffer and parse it
+const parsedToken = pagination.parseToken(Buffer.from(token, 'base64'));
+```
+
+
+Pass in an AES cipher Key return an encrypted token
+```javascript
+const { CursorPagination } = require('cursor-pagination');
+const pagination = new CursorPagination({
+  aesKey: '0000000000000000'
+});
+
+// Encrypted with the cipher key passed in
+const tokenData = pagination.createToken({
+  entries: [
+    { key: 'updatedAt', value: new Date() },
+    { key: 'id', value: 'abcdefg', asc: false }
+  ]
+});
+```
---
+++ src/index.ts
@@ -0,0 +1,140 @@
+import { PreciseDate } from '@google-cloud/precise-date';
+import { loadSync } from 'protobufjs';
+import { join } from 'path';
+import { createCipheriv, createDecipheriv, randomBytes } from 'crypto';
+
+
+
+// ===========================================================================================
+// --- Constants/Interfaces
+// ===========================================================================================
+const root = loadSync(join(__dirname, 'index.proto'));
+const PayloadType = root.lookupType('sommaht.cursor.v1.PageTokenPayload');
+
+const protoOptions = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: false,
+  oneofs: true
+};
+
+export interface PageTokenPayload {
+  entries: PageTokenEntry[]
+}
+
+export interface PageTokenEntry {
+  key: string;
+  value: number | string | Date | PreciseDate | boolean;
+  asc?: boolean;
+}
+
+interface ProtoPageTokenEntry {
+  key: string;
+  asc: boolean;
+  sValue?: string;
+  nValue?: number;
+  bValue?: boolean;
+  tValue?: {
+    seconds: number;
+    nanos: number;
+  }
+}
+
+interface ProtoPageTokenPayload {
+  entries: ProtoPageTokenEntry[];
+}
+
+export interface CursorPaginationConfig {
+  aesKey?: string;
+  aesAlgorithm?: string;
+}
+
+// ===========================================================================================
+// --- CursorPagination Class
+// ===========================================================================================
+export class CursorPagination {
+
+  constructor(private readonly config?: CursorPaginationConfig) {
+    this.config = Object.assign({ aesAlgorithm: 'aes-128-ctr' }, config);
+  }
+
+  // ===========================================================================================
+  // --- Public API
+  // ===========================================================================================
+  createToken(payload: PageTokenPayload): Buffer {
+    const protoPayload = this.convertPageTokenPayloadToProto(payload);
+    const data = this.encode(protoPayload);
+
+    return this.config.aesKey ? this.encrypt(data, this.config.aesKey, this.config.aesAlgorithm) : data;
+  }
+
+  parseToken(input: Buffer): PageTokenPayload {
+    const data = this.config.aesKey ? this.decrypt(input, this.config.aesKey, this.config.aesAlgorithm) : input;
+
+    const protoPayload = this.decode(data);
+    return this.convertPageTokenPayloadFromProto(protoPayload);
+  }
+
+  // ===========================================================================================
+  // --- Conversion Functions
+  // ===========================================================================================
+  private convertPageTokenPayloadToProto(payload: PageTokenPayload): ProtoPageTokenPayload {
+    return {
+      entries: payload.entries.map(this.convertPageTokenEntryToProto)
+    }
+  }
+
+  private convertPageTokenEntryToProto(entry: PageTokenEntry): ProtoPageTokenEntry {
+    let { key, asc, value } = entry;
+    if (typeof asc === 'undefined') asc = true;
+
+    if (typeof value === 'string') return { key, asc, sValue: value };
+    if (typeof value === 'number') return { key, asc, nValue: value };
+    if (typeof value === 'boolean') return { key, asc, bValue: value };
+
+    if (value instanceof Date) {
+      const date = (value instanceof PreciseDate) ? value : new PreciseDate(value);
+      return { key, asc, tValue: date.toStruct() }
+    }
+
+    throw new Error('CursorPagination - Unable to determine type of value');
+  }
+
+  private convertPageTokenPayloadFromProto(payload: ProtoPageTokenPayload): PageTokenPayload {
+    return {
+      entries: payload.entries.map(this.convertPageTokenEntryFromProto)
+    }
+  }
+
+  private convertPageTokenEntryFromProto(entry: ProtoPageTokenEntry): PageTokenEntry {
+    const { key, asc } = entry;
+    const value = entry.sValue || entry.nValue || entry.bValue || (entry.tValue && new PreciseDate(entry.tValue));
+
+    return { key, asc, value };
+  }
+
+  // ===========================================================================================
+  // --- Helper Functions
+  // ===========================================================================================
+  private encode(payload: ProtoPageTokenPayload): Buffer {
+    return <Buffer>PayloadType.encode(PayloadType.fromObject(payload)).finish();
+  }
+
+  private decode(data: Buffer): ProtoPageTokenPayload {
+    return <any>PayloadType.toObject(PayloadType.decode(data), protoOptions);
+  }
+
+  private encrypt(input: Buffer, cipherKey: string, algorithm: string): Buffer {
+    const iv = randomBytes(16);
+    const cipher = createCipheriv(algorithm, cipherKey, iv);
+    return Buffer.concat([iv, cipher.update(input), cipher.final()]);
+  }
+
+  private decrypt(data: Buffer, cipherKey: string, algorithm: string): Buffer {
+    // first 16 bits are the initialization vector, rest is encrypted data
+    const iv = data.slice(0, 16);
+    const decipher = createDecipheriv(algorithm, cipherKey, iv);
+    return Buffer.concat([decipher.update(data.slice(16)), decipher.final()]);
+  }
+}
